cf56a3a1c397b33c9d2b8c3050975fb101c173c6
natashabhuta <natasha.bhuta@gmail.com>
2018-08-09 19:24:13 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -2,7 +2,7 @@ def my_each([2, 4, 6, 8])
   my_each(array) do |num|
    while num < 5
    yield array[2, 4, 6, 8]
-   num = 
+   num = num + 1 
    
     if furniture.start_with?("d")
   puts "I have a #{furniture}"
