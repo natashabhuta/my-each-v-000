c854eddc5e576de2b5d82e4179dbc0b46167b14c
natashabhuta <natasha.bhuta@gmail.com>
2018-08-09 19:29:30 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,8 +1,9 @@
+
 def my_each(array)
-my_each( [2, 4, 6, 8]) do |num|
-   while num < 5
-   yield (my_each[num])
-   num = num + 1 
-    end
-    array
-end 
+  i = 0
+  while i < array.length
+    yield(array[i])
+    i += 1
+  end
+  array
+end
